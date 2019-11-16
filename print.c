332762ac0a84023e5c6f30c840fe9c43d3de2118
Ben Keener <keener4christ@gmail.com>
2019-11-16 01:17:43 +0000

    Added argument for exit

--- print.c
+++ print.c
@@ -72,3 +72,37 @@ int _strcmp(char *s1, char *s2)
 
 	return (1);
 }
+
+/**
+ * _atoi - Converts a string to an integer
+ * @s: pointer to the first character of the string
+ *
+ * Return: Value of integer in string
+ */
+int _atoi(char *s)
+{
+	unsigned int num;
+	int neg;
+
+	neg = 1;
+	num = 0;
+
+	for (; *s; s++)
+	{
+		if (*s >= '0' && *s <= '9')
+		{
+			num *= 10;
+			num += *s - '0';
+		}
+		else if (num > 0)
+		{
+			break;
+		}
+		else if (*s == '-')
+		{
+			neg = -neg;
+		}
+	}
+
+	return (num * neg);
+}
