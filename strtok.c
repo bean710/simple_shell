a764abd7ef0c2fc35b33a8f7795a9b6c0fda8dc7
Ben Keener <keener4christ@gmail.com>
2019-11-23 21:08:49 +0000

    Added docs for _strtok

--- strtok.c
+++ strtok.c
@@ -1,9 +1,17 @@
 #include "crikey.h"
 
+/**
+ * _strtok - Tokenizes a string by a character delimiter
+ * @src: The string to be tokenized on the first call, should be NULL for
+ * subsequent calls when tokenizing the same string
+ * @delim: The delimiter to tokenize by
+ *
+ * Return: Pointer to the beginning of the new token
+ */
 char *_strtok(char *src, char delim)
 {
-	static char *start = NULL;
-	static char *last_null = NULL;
+	static char *start;
+	static char *last_null;
 	char *ret = NULL;
 
 	if (src)
